e52ff8294d91526d6bc6a772658fc39326b8abed
etienne-bleuje <etin.jacob@gmail.com>
2024-05-06 19:32:30 +0200

    count setter: removed commented code

--- bin/data/computeshader_setter.glsl
+++ bin/data/computeshader_setter.glsl
@@ -6,12 +6,10 @@ uniform uint value;
 
 layout(std430,binding=3) buffer mutex
 {
-	uint m[];
+	uint particlesCounter[];
 };
 
 layout(local_size_x = 32, local_size_y = 32, local_size_z = 1) in;
 void main(){
-	m[ uint(gl_GlobalInvocationID.x * height + gl_GlobalInvocationID.y) ] = value;
-	//float dc = 0.75;
-	//m[ uint(gl_GlobalInvocationID.x * height + gl_GlobalInvocationID.y) ] = uint(floor(dc*float(m[ uint(gl_GlobalInvocationID.x * height + gl_GlobalInvocationID.y) ])));
+	particlesCounter[ uint(gl_GlobalInvocationID.x * height + gl_GlobalInvocationID.y) ] = value;
 }
