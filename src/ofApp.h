161ef1c56b03eb26eca819c777ca7838d00cd6fd
etienne-bleuje <etin.jacob@gmail.com>
2025-04-25 19:50:24 +0200

    wave bug fix

--- src/ofApp.h
+++ src/ofApp.h
@@ -63,7 +63,7 @@ public:
     void actionSwapParams();
     void actionRandomParams();
     void actionChangeColorMode();
-    void actionTriggerWave();
+    void actionTriggerWave(int gamepadIndex);
     void actionChangeDisplayType();
     void actionChangeSelectionIndex(int dir);
     void actionSpawnParticles(int spawnType);
