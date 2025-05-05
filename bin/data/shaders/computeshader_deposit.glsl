a0f102a64ca9ba811298b5a1acd96797a6858d3e
etienne-bleuje <etin.jacob@gmail.com>
2025-05-06 00:51:11 +0200

    solar color replaced by way cooler colors

--- bin/data/shaders/computeshader_deposit.glsl
+++ bin/data/shaders/computeshader_deposit.glsl
@@ -250,8 +250,9 @@ void main(){
 	}
 	else if(colorModeType == 1) // yellow
 	{
-		vec3 col1 = gradSolarDrift(tanh(countColorValue*1.3));
-		col = mix(col1,col2,blend);
+		vec3 col1 = gradPurpleFire(tanh(countColorValue*1.3));
+        vec3 col3 = gradArctic(tanh(countColorValue*1.3));
+		col = mix(col1,col3,blend);
 	}
 	else if(colorModeType == 2) // pink/purple (from z0rg :) )
 	{
