9c95b5df9eadaa7bdb8bc1892ed33ce0af657192
etienne-bleuje <etin.jacob@gmail.com>
2025-05-20 17:36:49 +0200

    critical bug fix for 2 players maangement (oldest active becomes active after both gamepads are inactive)

--- src/ofApp.h
+++ src/ofApp.h
@@ -79,6 +79,7 @@ public:
 
     int displayType = 1;
     int colorModeType = 1;
+    int previousNumberOfTrulyActiveGamepads = 0;
     std::array<float, 2> translationAxis1Array;
     std::array<float, 2> translationAxis2Array;
     std::array<float, 2> actionAreaSizeSigmaArray;
